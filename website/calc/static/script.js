1b009c26cc8334890762cdce63bfa8cc6a6a0ff4
derickDarsh <aladakattidarshan@gmail.com>
2022-04-15 17:13:01 +0530

    first commit

---
+++ website/calc/static/script.js
@@ -0,0 +1,17 @@
+var counter = 0;
+
+var settingsMenu = document.querySelector(".settings-menu");
+
+function likebtn() {
+    counter++;
+    document.getElementById("votes").innerHTML = counter;
+}
+
+function dislikebtn() {
+    counter--;
+    document.getElementById("votes_1").innerHTML = counter;
+}
+
+function settingsMenuToggle(){
+    settingsMenu.classList.toggle("settings-menu-height");
+}
---
+++ website/calc/views.py
@@ -0,0 +1,149 @@
+from audioop import reverse
+from pickle import APPEND
+from urllib import request
+from django.shortcuts import render
+import mysql.connector
+# from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+
+def index_login(request):
+    return render(request,'login.html')
+
+def login(request): 
+    if request.method=="POST":
+        name=request.POST['fullName']
+        username=request.POST['userName']
+        email=request.POST['eMail']
+        pwd=request.POST['passWord']
+
+        conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+        cursor=conn.cursor()
+
+        cursor.execute("insert into project.users(name,username,email,password) values(%s,%s,%s,%s)",(name,username,email,pwd))
+
+        return render(request,'login.html')
+
+    else:    
+        return render(request,'signup.html')
+
+
+def home_feed(request):
+    conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+    cursor1=conn.cursor()
+
+    cursor1.execute("select questions from project.questions")
+    data=cursor1.fetchall()
+
+    if id is not None:
+            conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+            cursor1=conn.cursor()
+
+            cursor1.execute("select username,questions from project.questions")
+            data=cursor1.fetchall()
+
+            str(data)
+            # list_questions = [lis[0] for lis in data]
+            # list_questions.reverse()
+            # list_questions=str(list_tuple_questions)
+
+            data.reverse()
+            return render(request,'feed.html',{'result':data})
+
+
+
+
+def feed(request):
+    if request.method=="POST":
+        uname=request.POST['username']
+        ps=request.POST['userpassword']
+
+        conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+        cursor=conn.cursor()
+
+        cursor.execute("select * from project.users where username=%s and password=%s",(uname,ps))
+        id=cursor.fetchone()
+
+        if id is not None:
+            conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+            cursor1=conn.cursor()
+
+            cursor1.execute("select username,questions from project.questions")
+            data=cursor1.fetchall()
+
+            str(data)
+            print(data)
+            # list_users= [li[0] for li in data]
+            # print(list_users)
+            # list_questions = [lis[1] for lis in data]
+            # list_users.reverse()
+            # list_questions.reverse()
+            data.reverse()
+            return render(request,'feed.html',{'result':data})
+
+        answer=request.POST['answer']
+        if answer is None:
+            return render(request,'feed.html',{'result':data})
+        else :
+            cursor1.execute("select user_id from project.users where username=%s",(uname,))
+            uid=cursor1.fetchone()
+            return render(request,'feed.html',{'result':data})
+            
+        # else:
+        #     return render(request,"login.html")
+
+
+def comment(request):
+    if request.method=="POST":
+        conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+        cursor1=conn.cursor()
+
+        cursor1.execute("select username,questions from project.questions")
+        data=cursor1.fetchall()
+        
+        answer=request.POST['answer']
+        if answer is None:
+            return render(request,'feed.html',{'result':data})
+        else :
+            # cursor1.execute("select user_id from project.users where username=%s",(uname,))
+            uid=cursor1.fetchone()
+            return render(request,'feed.html',{'result':data})
+
+
+def profile(request):
+    profile_name = 'John'
+    return render(request,'profile.html',{'Profile_name':profile_name})
+
+def notification(request):
+    return render(request,'notification.html')
+
+def settings(request):
+    return render(request,'settings.html')
+
+def add(request):
+    if request.method=="POST":
+        uname=request.POST['uname']
+        ques=request.POST['question']
+
+        conn= mysql.connector.connect(host='localhost',user='root',password='2307@darsh',database='project',autocommit=True)
+        cursor=conn.cursor()
+
+        cursor.execute("select user_id from project.users where username=%s",(uname,))
+        id=cursor.fetchone()
+        u_id=int(''.join(map(str,id)))
+
+        cursor.execute("insert into project.questions(username,user_id,questions) VALUES (%s,%s,%s)",(uname,u_id,ques))
+
+        return render(request,"add.html")
+    else:
+        return render(request,'add.html')
+
+def search_q(request):
+    return render(request,'search.html')
+
